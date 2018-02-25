28981c55b4e23c7a4dd7caabf57c2ab28634dc15
Marcus Paulo <mpaulobr@gmail.com>
2018-02-25 20:56:51 -0300

    Commit Inicial do Projeto (Simples) de Gestão de Patrimônio. Desenvolvido em Angular. Workshop promovido pela AlgaWorks em Fevereiro de 2018

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # PatrimonioUi
-
+#Projeto Simples para Gestão de Patrimônio. Workshop da Algaworks em Fevereiro de 2018. Projeto desenvolvido em Angular e Spring Boot.
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.1.
+
+## Install dependencies
+Run `npm install`
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgForm, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ItemService } from './item.service';
 
+import { DataTableModule, ButtonModule, InputTextModule, InputMaskModule, CalendarModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DataTableModule,
+    ButtonModule, 
+    InputTextModule, 
+    InputMaskModule, 
+    CalendarModule
   ],
-  providers: [],
+  providers: [
+    ItemService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
